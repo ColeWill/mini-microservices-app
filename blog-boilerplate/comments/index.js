1bc2aad25dc80a992e60ecc63f823455d54652c3
cowilliams_deloitte <cowilliams@deloitte.com>
2024-09-06 13:37:51 -0600

    posts and comments routes working

---
+++ blog-boilerplate/comments/index.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const {randomBytes} = require('crypto');
+
+const app = express();
+app.use(bodyParser.json());
+
+const commentsByPostId = {};
+
+app.get('/posts/:id/comments', (req,res) => {
+    res.send(commentsByPostId[req.params.id] || []);
+});
+
+app.post('/posts/:id/comments', (req,res) => {
+    console.log('1111', req.body, req.params)
+    const commentsId = randomBytes(4).toString('hex');
+    const {content} = req.body;
+    const comments = commentsByPostId[req.params.id] || [];
+    comments.push({id: commentsId, content})
+    commentsByPostId[req.params.id] = comments;
+    res.status(201).send(comments)
+});
+
+app.listen(4001, () => {
+    console.log('listening on port 4001');
+});
